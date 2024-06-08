3c0886137f225749ce163d8fd6851e12c39fd12d
andgram <145982786+andgram@users.noreply.github.com>
2024-06-08 20:12:24 +0200

    make anchor a button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function smoothScroll() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    document.querySelectorAll('button[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
             const target = document.querySelector(this.getAttribute('href'));
@@ -7,14 +7,11 @@ function smoothScroll() {
                 target.scrollIntoView({
                     behavior: 'smooth'
                 });
-            } else {
-                console.error('Target element not found:', this.getAttribute('href'));
-            }
+            } 
         });
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM fully loaded and parsed');
     smoothScroll();
 });
